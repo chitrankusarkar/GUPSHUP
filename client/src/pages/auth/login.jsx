432150dedad4ae1e8d007a2d61c0a8554c98a8b9
Chitranku <chitrankusarkar1@gmail.com>
2025-03-04 04:31:34 +0530

    Created Login and Signup

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div className="min-h-screen flex justify-center items-center p-5">
+        <div className="min-h-screen w-full flex justify-center items-center p-5">
             <div className="max-w-[50rem] flex flex-col gap-2 bg-base-200 p-6 rounded-lg">
                 <h2 className="text-2xl text-center pb-6">Please Login :)</h2>
                 <label class="input input-bordered flex items-center gap-2">
@@ -14,9 +14,9 @@ const Login = () => {
                 </label>
                 <label class="input input-bordered flex items-center gap-2">
                     <TbLockPassword />
-                    <input type="password" class="grow" />
+                    <input type="password" class="grow" placeholder="Password"/>
                 </label>
-                <button class="btn btn-dash">Submit</button>
+                <button class="btn btn-dash">Login</button>
                 <p>
                     Don't have an account? &nbsp;
                     <Link to='/signup' className="underline">Sign Up</Link>
--- client/src/pages/auth/signup.jsx
+++ client/src/pages/auth/signup.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import { FaUser } from "react-icons/fa";
+import { TbLockPassword } from "react-icons/tb";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
     return (
-        <div>SignUp</div>
+        <div className="min-h-screen flex justify-center items-center p-5">
+            <div className="max-w-[50rem] w-full flex flex-col gap-2 bg-base-200 p-6 rounded-lg">
+                <h2 className="text-2xl text-center pb-6">Please Signup :)</h2>
+                <label class="input input-bordered flex items-center gap-2">
+                    <FaUser />
+                    <input type="text" class="grow" placeholder="Full Name" />
+                </label>
+                <label class="input input-bordered flex items-center gap-2">
+                    <FaUser />
+                    <input type="text" class="grow" placeholder="username" />
+                </label>
+                <label class="input input-bordered flex items-center gap-2">
+                    <TbLockPassword />
+                    <input type="password" class="grow" placeholder="Password"/>
+                </label>
+                <label class="input input-bordered flex items-center gap-2">
+                    <TbLockPassword />
+                    <input type="password" class="grow" placeholder="Confirm Password"/>
+                </label>
+                <button class="btn btn-dash">Signup</button>
+                <p>
+                    Already have an account? &nbsp;
+                    <Link to='/login' className="underline">Log In</Link>
+                </p>
+            </div>
+        </div>
     )
 }
 
