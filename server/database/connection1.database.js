a21a3aba8baa12d48bbd5ee2216a7738e4323c78
Chitranku <chitrankusarkar1@gmail.com>
2025-04-28 19:29:19 +0530

    DB Work

--- server/database/connection1.database.js
+++ server/database/connection1.database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const MONGODB_URI = process.env.MONGODB_URI;
-        const instance = await mongoose.connect("mongodb+srv://sarkarriju42:Riju1234@cluster0.pmy19tr.mongodb.net/");
+        const instance = await mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log(`MongoDB Connected: ${instance.connection.host}`);
     } catch (err) {
         console.error('Error connecting to MongoDB:', err.message);
