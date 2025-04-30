e2e748d39386728b54419f9e9764dc0a1cc219d2
Chitranku <chitrankusarkar1@gmail.com>
2025-04-30 23:32:38 +0530

    Fixed db con till now

--- server/models/conversation.model.js
+++ server/models/conversation.model.js
@@ -15,5 +15,5 @@ const conversationSchema = new mongoose.Schema({
     ],
 }, { timestamps: true })
 
-const Conversation = mongoose.model("Conversation", conversationSchemaSchema);
+const Conversation = mongoose.model("Conversation", conversationSchema);
 export default Conversation;
