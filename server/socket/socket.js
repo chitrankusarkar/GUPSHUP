3e355499c4b2565e17ab2e67463b9aafac639fb7
Chitranku Sarkar <109690584+chitrankusarkar@users.noreply.github.com>
2025-05-23 17:16:09 +0530

    Update socket.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://gupshup-inky.vercel.app"
+        origin: "https://gupshup-lh4u.onrender.com/"
     }
 })
 
