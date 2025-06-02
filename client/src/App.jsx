9974d8545387e846b87bb66d60e46708c07bedce
Chitranku <chitrankusarkar1@gmail.com>
2025-06-03 02:01:54 +0530

    Fix app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,38 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from "react"
-import { getOtherUsersThunk, getUserProfileThunk } from "./store/slice/user/user.thunk.js"
-import { Outlet } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
+import "./App.css";
+import { Toaster } from 'react-hot-toast';
+import { Outlet } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import { getOtherUsersThunk, getUserProfileThunk } from "./store/slice/user/user.thunk.js";
 
 function App() {
-  const dispatch = useDispatch()
-  const { screenLoading } = useSelector(state => state.userReducer)
+  const dispatch = useDispatch();
+  const { screenLoading } = useSelector(state => state.userReducer);
+  const [authChecked, setAuthChecked] = useState(false); // ⏳ wait for auth before routing
 
   useEffect(() => {
     (async () => {
-      await dispatch(getUserProfileThunk())
-      await dispatch(getOtherUsersThunk())
-    })()
-  }, [])
+      await dispatch(getUserProfileThunk());
+      await dispatch(getOtherUsersThunk());
+      setAuthChecked(true);
+    })();
+  }, []);
 
-  if (screenLoading) {
+  if (screenLoading || !authChecked) {
     return (
       <div className="h-screen w-full flex flex-col justify-center items-center bg-gray-900 text-white animate-fadeIn">
         <h1 className="text-4xl font-bold">GUPSHUP</h1>
         <p className="text-lg mt-2">Your personalized chat experience</p>
       </div>
-    )
+    );
   }
 
   return (
-    <>
-      <Toaster position="top-center" />
+    <div>
+      <Toaster position="top-center" reverseOrder={false} />
       <Outlet />
-    </>
-  )
+    </div>
+  );
 }
-export default App
+
+export default App;
