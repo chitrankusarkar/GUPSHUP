236d98a08ee9465d6041af98a28429c439f3158f
Chitranku Sarkar <109690584+chitrankusarkar@users.noreply.github.com>
2025-06-03 01:25:24 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import "./App.css"
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from "react"
+import { getOtherUsersThunk, getUserProfileThunk } from "./store/slice/user/user.thunk.js"
+import { Outlet } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import { Outlet } from 'react-router-dom';
-import { useDispatch } from 'react-redux'
-import { useEffect } from "react";
-import { getOtherUsersThunk, getUserProfileThunk } from "./store/slice/user/user.thunk.js";
 
 function App() {
   const dispatch = useDispatch()
+  const { screenLoading } = useSelector(state => state.userReducer)
+
   useEffect(() => {
     (async () => {
       await dispatch(getUserProfileThunk())
@@ -14,15 +15,20 @@ function App() {
     })()
   }, [])
 
+  if (screenLoading) {
+    return (
+      <div className="h-screen w-full flex flex-col justify-center items-center bg-gray-900 text-white animate-fadeIn">
+        <h1 className="text-4xl font-bold">GUPSHUP</h1>
+        <p className="text-lg mt-2">Your personalized chat experience</p>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-      />
+    <>
+      <Toaster position="top-center" />
       <Outlet />
-    </div>
+    </>
   )
 }
-
-export default App;
+export default App
