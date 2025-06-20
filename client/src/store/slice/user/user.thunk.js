e3d012057b020d04575fc63fbf36ccd0d4db600b
Chitranku <chitrankusarkar1@gmail.com>
2025-06-21 02:40:23 +0530

    add msg status

--- client/src/store/slice/user/user.thunk.js
+++ client/src/store/slice/user/user.thunk.js
@@ -2,8 +2,22 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../../components/utilities/axiosinstance.js";
 import { toast } from "react-hot-toast";
 import { initializeSocket } from "../socket/socket.slice.js";
-import { getOtherUsersThunk } from "./user.thunk.js";
 
+// ✅ Get Other Users
+export const getOtherUsersThunk = createAsyncThunk(
+  "user/getOtherUsers",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.get("/user/get-other-users");
+      return response.data;
+    } catch (error) {
+      const errorOutput = error?.response?.data?.errMessage;
+      return rejectWithValue(errorOutput);
+    }
+  }
+);
+
+// ✅ Login
 export const loginUserThunk = createAsyncThunk(
   "user/login",
   async ({ username_or_email, password }, { dispatch, rejectWithValue }) => {
@@ -25,6 +39,7 @@ export const loginUserThunk = createAsyncThunk(
   }
 );
 
+// ✅ Sign Up
 export const signUpUserThunk = createAsyncThunk(
   "user/signup",
   async ({ fullName, username, email, password, gender }, { dispatch, rejectWithValue }) => {
@@ -49,6 +64,7 @@ export const signUpUserThunk = createAsyncThunk(
   }
 );
 
+// ✅ Logout
 export const logoutUserThunk = createAsyncThunk(
   "user/logout",
   async (_, { rejectWithValue }) => {
@@ -63,6 +79,7 @@ export const logoutUserThunk = createAsyncThunk(
   }
 );
 
+// ✅ Get Profile
 export const getUserProfileThunk = createAsyncThunk(
   "user/getProfile",
   async (_, { dispatch, rejectWithValue }) => {
@@ -72,19 +89,6 @@ export const getUserProfileThunk = createAsyncThunk(
       dispatch(initializeSocket(response.data.responseData._id));
       dispatch(getOtherUsersThunk());
 
-      return response.data;
-    } catch (error) {
-      const errorOutput = error?.response?.data?.errMessage;
-      return rejectWithValue(errorOutput);
-    }
-  }
-);
-
-export const getOtherUsersThunk = createAsyncThunk(
-  "user/getOtherUsers",
-  async (_, { rejectWithValue }) => {
-    try {
-      const response = await axiosInstance.get("/user/get-other-users");
       return response.data;
     } catch (error) {
       const errorOutput = error?.response?.data?.errMessage;
