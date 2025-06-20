119283fa83efcc9f65c39f7ae08d5316450296ec
Chitranku <chitrankusarkar1@gmail.com>
2025-06-21 02:12:19 +0530

    Fix chat time

--- client/src/pages/home/Message.jsx
+++ client/src/pages/home/Message.jsx
@@ -14,6 +14,20 @@ const Message = ({ messageDetails }) => {
 
     const isSender = userProfile?._id === messageDetails?.senderId;
     const avatar = isSender ? userProfile?.avatar : selectedUser?.avatar;
+    const formatMessageTime = (timestamp) => {
+        const now = moment();
+        const time = moment(timestamp);
+
+        if (now.isSame(time, 'day')) {
+            return time.format('h:mm A'); 
+        } else if (now.clone().subtract(1, 'day').isSame(time, 'day')) {
+            return 'Yesterday';
+        } else if (now.diff(time, 'days') < 7) {
+            return time.format('dddd'); 
+        } else {
+            return time.format('MMM D'); 
+        }
+    };
 
     return (
         <div
@@ -27,7 +41,7 @@ const Message = ({ messageDetails }) => {
             </div>
             <div className="chat-header">
                 <time className="text-xs opacity-50">
-                    {moment(messageDetails?.createdAt).format("h:mm A")} {/* ✅ formatted time */}
+                    {formatMessageTime(messageDetails?.createdAt)} 
                 </time>
             </div>
             <div className="chat-bubble">{messageDetails?.message}</div>
