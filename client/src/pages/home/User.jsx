1c3fdd8f737380bb287d81cb8437fa12e4020124
Chitranku <chitrankusarkar1@gmail.com>
2025-06-22 16:52:02 +0530

    useronlinefix

--- client/src/pages/home/User.jsx
+++ client/src/pages/home/User.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSelectedUser } from "../../store/slice/user/user.slice";
+
 const User = ({ userDetails, onCloseMobile = () => {}, isStatic = false }) => {
   const dispatch = useDispatch();
   const { selectedUser } = useSelector((state) => state.userReducer);
+  const { onlineUsers } = useSelector((state) => state.socketReducer);
 
   const handleUserSelect = () => {
     dispatch(setSelectedUser(userDetails));
     onCloseMobile();
   };
 
+  const isOnline = onlineUsers?.includes(userDetails._id);
+
   return (
     <div
       onClick={!isStatic ? handleUserSelect : undefined}
       className={`flex items-center gap-4 p-3 hover:bg-gray-700 cursor-pointer ${
-        selectedUser?._id === userDetails?._id && "bg-gray-700"
+        !isStatic && selectedUser?._id === userDetails?._id ? "bg-gray-700" : ""
       }`}
     >
-      <img
-        src={userDetails?.avatar}
-        alt="avatar"
-        className="w-10 h-10 rounded-full object-cover"
-      />
+      <div className="relative">
+        <img
+          src={userDetails?.avatar}
+          alt="avatar"
+          className="w-10 h-10 rounded-full object-cover"
+        />
+        {isOnline && (
+          <span className="absolute bottom-0 right-0 block w-3 h-3 bg-green-400 border-2 border-gray-900 rounded-full"></span>
+        )}
+      </div>
       <div className="flex flex-col">
         <p className="text-white font-medium">{userDetails?.fullName}</p>
         <p className="text-white/60 text-sm">@{userDetails?.username}</p>
