233871bd17fbe1b757aae1c1c66596ecc7953a58
Chitranku <chitrankusarkar1@gmail.com>
2025-06-22 17:19:59 +0530

    typing status added

--- client/src/pages/home/SendMessage.jsx
+++ client/src/pages/home/SendMessage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { IoIosSend } from "react-icons/io";
 import { useDispatch, useSelector } from "react-redux";
 import { sendMessageThunk } from "../../store/slice/message/message.thunk";
@@ -6,12 +6,28 @@ import { sendMessageThunk } from "../../store/slice/message/message.thunk";
 const SendMessage = () => {
   const dispatch = useDispatch();
   const { selectedUser } = useSelector((state) => state.userReducer);
+  const { socket } = useSelector((state) => state.socketReducer);
+
   const [message, setMessage] = useState("");
+  const typingTimeout = useRef(null);
 
   const handleSendMessage = () => {
     if (!message.trim() || !selectedUser?._id) return;
     dispatch(sendMessageThunk({ receiverId: selectedUser._id, message }));
     setMessage("");
+
+    if (socket) socket.emit("stopTyping", { to: selectedUser._id });
+  };
+
+  const handleTyping = () => {
+    if (!socket || !selectedUser?._id) return;
+
+    socket.emit("typing", { to: selectedUser._id });
+
+    if (typingTimeout.current) clearTimeout(typingTimeout.current);
+    typingTimeout.current = setTimeout(() => {
+      socket.emit("stopTyping", { to: selectedUser._id });
+    }, 2000); 
   };
 
   return (
@@ -22,7 +38,10 @@ const SendMessage = () => {
           placeholder="Type here..."
           className="flex-grow bg-transparent input-md outline-none text-white p-2"
           value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          onChange={(e) => {
+            setMessage(e.target.value);
+            handleTyping();
+          }}
           onKeyDown={(e) => {
             if (e.key === "Enter") handleSendMessage();
           }}
